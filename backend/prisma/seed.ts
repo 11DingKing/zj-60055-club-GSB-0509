98cd733b6e18bae7ba039d1f849d1bae5f561e8d
ding <ding@ding.com>
2026-05-10 10:53:34 +0800

    本次提交不计入轮次

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,5 +1,5 @@
 import { PrismaClient, UserRole, EventType, EventStatus } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
